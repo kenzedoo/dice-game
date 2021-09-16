8568e68f82788577428553cf4ed99718219f9d5c
fensomer <reece.fensome@flooid.com>
2021-09-16 17:21:36 +0100

    Working implementation without top 5

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import random
 # GitHub Access Token: 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
 
 
-authed_players = ["Tom", "Mac", "Seth", "Kyle"]
-winner_name = ""
-players = {}
-num_of_games = 5
-total_scores = {}
+authed_players = ["Tom", "Mac", "Seth", "Kyle"] #Array
+winner_name = "" #String
+players = {} #Map
+num_of_games = 5 #Int
+total_scores = {} #Map
 
 
 # function to sign in user
@@ -23,9 +23,9 @@ def sign_in():
 
 # function to initialise game for current user
 def play_game(current_player):
-    print("Do you want to roll y/n", current_player)
+    print("Do you want to roll {}? (Y/n)".format(current_player))
     player_input = input()
-    if player_input == "y":
+    if player_input.lower() == "y":
         players[current_player].append(roll_dice(current_player))
     else:
         print("Too bad sucker")
@@ -68,6 +68,7 @@ while len(players.keys()) < 2:
 
 # keep looping games for num_of_games for each player
 for game in range(num_of_games):
+    print("Starting game: {}".format(game+1))
     for player in players.keys():
         play_game(player)
 
@@ -75,12 +76,27 @@ for game in range(num_of_games):
 for player in players.keys():
     total_scores[player] = sum(players[player])
 
+
 # get winning scores
 winners = [key for m in [max(total_scores.values())] for key, val in total_scores.items() if val == m]
 
 # if winners are greater than 1 then game was a draw else print winner
 if len(winners) > 1:
     print("Its time to d d d d d d d duel")
+    duel_scores = 0
+    current_player = []
+    for player in players.keys():
+        current_player.append(player)
+        roll = random.randint(1, 6)
+        print("{} has rolled {}".format(player, roll))
+        if duel_scores != 0:
+            if duel_scores == roll:
+                print("The game is a draw!")
+            elif duel_scores < roll:
+                print("The winner is {} with a score of {}".format(player, total_scores[player]))
+            else:
+                print("The winner is {} with a score of {}".format(current_player[0], total_scores[current_player[0]]))
+        duel_scores = roll
 else:
     with open("scores.txt", "a") as file:
         print("The winner is {} with a score of {}".format(winners[0], total_scores[winners[0]]))
